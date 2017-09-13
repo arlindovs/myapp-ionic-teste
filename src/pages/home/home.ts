b0bacc614b049e29497b2de90f0f7e69cfdfe397
Arlindo Silva <arlindovicente@gmail.com>
2017-09-12 22:48:32 -0300

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  botao_entrar = LoginPage;
 
   constructor(public navCtrl: NavController) {
 
